11169f3dfb8d42cd62301a7572b126b84c1a2f5c
Carolyn Yates <2carolynyates@gmail.com>
2015-08-10 12:33:37 +0100

    links to blog pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
